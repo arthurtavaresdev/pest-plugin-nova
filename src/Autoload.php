e1c1cb2cc638b1c1b7dc6daefe825e9451d70f30
Arthur Tavares <arthurabreu00@gmail.com>
2022-02-21 14:54:02 -0300

    Revert "write proprieties"
    This reverts commit 5edb8bd7fbbdf74c9601cbf60cd1f2a9c1523b10.

--- src/Autoload.php
+++ src/Autoload.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace ArthurTavaresDev\PestPluginNova;
 
+use NovaTesting\NovaAssertions;
 use Pest\Plugin;
+
 
 // @codeCoverageIgnoreStart
 Plugin::uses(NovaAssertions::class);
 // @codeCoverageIgnoreEnd
 
-require_once 'NovaExpectations.php';
+
+require_once  'NovaExpectations.php';
